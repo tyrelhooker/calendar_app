8b539fd58f2dd9e31b489281208c7aa8df20535e
TJ Hooker <tyrelhooker@gmail.com>
2019-04-11 21:46:38 -0400

    Create javascript code determining month and dynamically creating days in html

---
+++ script.js
@@ -0,0 +1,35 @@
+let select = document.querySelector('select');
+let list = document.querySelector('ul');
+let h2 = document.querySelector('h2');
+
+select.onchange = function() {
+  let choice = select.value;
+  let days;
+
+  // ADD CONDITIONAL HERE
+  if (choice === 'February') {
+    days = 28;
+  } else if (choice === 'September' || 'April' || 'June' || 'November') {
+    days = 30;
+  } else {
+    days = 31;
+  }
+
+  createCalendar(days, choice);
+}
+
+function createCalendar(days, choice) {
+  list.innerHTML = '';
+  h2.textContent = choice;
+  for (let i = 1; i <= days; i++) {
+    let listItem = document.createElement('li');
+    listItem.textContent = i;
+    list.appendChild(listItem);
+  }
+}
+
+createCalendar(31,'January');
+
+
+
+
